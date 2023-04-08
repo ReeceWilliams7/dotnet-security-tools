80cfbbfcad3a7e29e70ded91c402866b78e66ff8
Reece <reece.williams7@gmail.com>
2023-04-08 20:24:12 +0100

    Fixing client credentials page

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -2,19 +2,17 @@ using RW7.DotNetSecurityTools.ClientCredentials;
 using RW7.DotNetSecurityTools.JsonWebKeys;
 using RW7.DotNetSecurityTools.RsaSecurityKeys;
 
-using WebApp.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 builder.Services.AddSingleton<IRsaSecurityKeyCreator, RsaSecurityKeyCreator>();
-
 builder.Services.AddSingleton<IJsonWebKeyCreator, JsonWebKeyCreator>();
 
+builder.Services.AddSingleton<IClientIdGenerator, GuidClientIdGenerator>();
+builder.Services.AddSingleton<IClientSecretGenerator, Base64RngCryptoClientSecretGenerator>();
 builder.Services.AddSingleton<IClientCredentialsCreator, ClientCredentialsCreator>();
 
 var app = builder.Build();
