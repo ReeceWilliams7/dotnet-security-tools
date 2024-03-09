30265f5a32b4cebacf577bf26f5ed15d1a983668
Reece <reece.williams7@gmail.com>
2024-03-09 12:31:41 +0000

    Use camel casing for output

--- src/JsonWebKeys/Models/JsonWebKeyOutput.cs
+++ src/JsonWebKeys/Models/JsonWebKeyOutput.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace RW7.DotNetSecurityTools.JsonWebKeys.Models
 {
     public class JsonWebKeyOutput
     {
+
         public JsonWebKeyOutput(JsonWebKey jsonWebKey, string rsaPublicKey, string rsaPrivateKey)
         {
             JsonWebKey = jsonWebKey;
@@ -17,7 +19,24 @@ namespace RW7.DotNetSecurityTools.JsonWebKeys.Models
 
         public JsonWebKey JsonWebKey { get; }
 
-        public string JsonWebKeyString => JsonConvert.SerializeObject(JsonWebKey, Formatting.Indented);
+        public string JsonWebKeyString { 
+            get
+            {
+                var contractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                };
+
+                var serializerSettings = new JsonSerializerSettings
+                {
+                    ContractResolver = contractResolver,
+                    Formatting = Formatting.Indented,
+                    NullValueHandling = NullValueHandling.Ignore,
+                };
+
+                return JsonConvert.SerializeObject(JsonWebKey, serializerSettings);
+            }
+        }
 
         public string Base64JsonWebKey => IdentityModel.Base64Url.Encode(Encoding.UTF8.GetBytes(JsonWebKeyString));
 
